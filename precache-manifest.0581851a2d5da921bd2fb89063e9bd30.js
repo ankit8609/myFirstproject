bb53c1a75028782ece4dc5b500f2cf6e7f9359f5
ankit8609 <87761962+ankit8609@users.noreply.github.com>
2022-03-17 14:29:30 +0530

    Updates

---
+++ precache-manifest.0581851a2d5da921bd2fb89063e9bd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0182c96b742d763e120c",
+    "url": "/myFirstproject/static/css/main.d50af24b.chunk.css"
+  },
+  {
+    "revision": "0182c96b742d763e120c",
+    "url": "/myFirstproject/static/js/main.0edd5bfc.chunk.js"
+  },
+  {
+    "revision": "954127b73602131466df",
+    "url": "/myFirstproject/static/js/runtime~main.f113eba9.js"
+  },
+  {
+    "revision": "ee7f37ea120d5f770ab3",
+    "url": "/myFirstproject/static/js/2.18feb4ae.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/myFirstproject/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "b34d70b020c5381f011494e200c5f966",
+    "url": "/myFirstproject/index.html"
+  }
+];
